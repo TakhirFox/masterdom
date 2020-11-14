dc85cddbb78ceaff9a5cab6a1ec40dd7b64b9f9c
Takhir <winzero.nexis@mail.ru>
2020-11-14 15:26:04 +0700

    Initial Commit

---
+++ masterdom/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  masterdom
+//
+//  Created by Zakirov Tahir on 14.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
